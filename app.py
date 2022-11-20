f0e7f94ce7d9d45c7f01f335c975ff05b84481a8
zeeshansalim1234 <zeeshan.chougle@ucalgary.ca>
2022-11-20 02:54:07 -0800

    updated

--- app.py
+++ app.py
@@ -4,11 +4,6 @@ from flask_cors import CORS
 import pandas as pd
 import os 
 
-cred = credentials.Certificate("key.json")
-initialize_app(cred)
-db = firestore.client()
-user_Ref = db.collection('user')
-
 app = Flask(__name__)
 CORS(app)
 
@@ -45,102 +40,103 @@ def checkFoodData():
 
 
 def calculateRebateOnAnItem(barcode):
-    loc = 'Products.csv'
-    df = pd.read_csv(loc)
+    pass
+    # loc = 'Products.csv'
+    # df = pd.read_csv(loc)
 
-    cols = ['Brand','Alcoholic?','Container code','Size (ml)','UPC']
+    # cols = ['Brand','Alcoholic?','Container code','Size (ml)','UPC']
 
-    df.pop('Alcoholic?')
-    df.pop('Flavour')
-    df.pop('Container Code')
+    # df.pop('Alcoholic?')
+    # df.pop('Flavour')
+    # df.pop('Container Code')
 
-    # Addition of rebate values
+    # # Addition of rebate values
 
-    # Add Rebate Val as Column to the dataframe 
-    thresHold = 1000
+    # # Add Rebate Val as Column to the dataframe 
+    # thresHold = 1000
 
-    lowVal_AL = 0.9
-    highVal_AL = 0.25-0.09
+    # lowVal_AL = 0.9
+    # highVal_AL = 0.25-0.09
 
-    lowVal_PET = 0.10-0.03
-    highVal_PET = 0.25-0.07
+    # lowVal_PET = 0.10-0.03
+    # highVal_PET = 0.25-0.07
 
-    lowVal_Plastic = lowVal_PET
-    highVal_Plastic = highVal_PET
+    # lowVal_Plastic = lowVal_PET
+    # highVal_Plastic = highVal_PET
 
-    lowVal_Gl = 0.10-0.08
-    highVal_Gl = 0.25-0.08
+    # lowVal_Gl = 0.10-0.08
+    # highVal_Gl = 0.25-0.08
 
-    lowVal_Met = 0.10-0.09
-    highVal_Met = 0.25-0.09
+    # lowVal_Met = 0.10-0.09
+    # highVal_Met = 0.25-0.09
 
-    lowVal_Pouche = 0.10-0.04
-    highVal_Pouche = 0.25-0.04
+    # lowVal_Pouche = 0.10-0.04
+    # highVal_Pouche = 0.25-0.04
 
-    # Looping through the dataframe 
+    # # Looping through the dataframe 
 
-    rebateList = []
+    # rebateList = []
 
-    MaterialList = ['Glass','Aluminum','Bag In a Box','PET', 'Non-refillable',
-                    'Plastic - One-Way Keg','Tetra Brik','Drink-Pouch']
+    # MaterialList = ['Glass','Aluminum','Bag In a Box','PET', 'Non-refillable',
+    #                 'Plastic - One-Way Keg','Tetra Brik','Drink-Pouch']
 
-    idx = 0
-    for i in range(0,len(df)):
-        if (df['Size (ml)'][i] <thresHold):
-            if(df['Material'][i] == MaterialList[0]):
-                rebateList.append(lowVal_Gl)
-            elif df['Material'][i] == MaterialList[1]:
-                rebateList.append(lowVal_AL)
-            elif df['Material'][i] == MaterialList[2]:
-                rebateList.append(lowVal_Pouche)
-            elif df['Material'][i] == MaterialList[3]:
-                rebateList.append(lowVal_PET)
-            elif df['Material'][i] == MaterialList[4]:
-                rebateList.append(lowVal_PET)
-            elif df['Material'][i] == MaterialList[5]:
-                rebateList.append(lowVal_Plastic)
-            elif df['Material'][i] == MaterialList[6]:
-                rebateList.append(lowVal_PET)
-            else:
-                rebateList.append(lowVal_Pouche)
-        else:
-            if df['Material'][i] == MaterialList[2]:
-                rebateList.append(highVal_Pouche)
-            elif df['Material'][i] == MaterialList[3]:
-                rebateList.append(highVal_PET)
-            elif df['Material'][i] == MaterialList[4]:
-                rebateList.append(highVal_PET)
-            elif df['Material'][i] == MaterialList[5]:
-                rebateList.append(highVal_Plastic)
-            elif df['Material'][i] == MaterialList[6]:
-                rebateList.append(highVal_PET)
-            else :
-                rebateList.append(highVal_Pouche)
+    # idx = 0
+    # for i in range(0,len(df)):
+    #     if (df['Size (ml)'][i] <thresHold):
+    #         if(df['Material'][i] == MaterialList[0]):
+    #             rebateList.append(lowVal_Gl)
+    #         elif df['Material'][i] == MaterialList[1]:
+    #             rebateList.append(lowVal_AL)
+    #         elif df['Material'][i] == MaterialList[2]:
+    #             rebateList.append(lowVal_Pouche)
+    #         elif df['Material'][i] == MaterialList[3]:
+    #             rebateList.append(lowVal_PET)
+    #         elif df['Material'][i] == MaterialList[4]:
+    #             rebateList.append(lowVal_PET)
+    #         elif df['Material'][i] == MaterialList[5]:
+    #             rebateList.append(lowVal_Plastic)
+    #         elif df['Material'][i] == MaterialList[6]:
+    #             rebateList.append(lowVal_PET)
+    #         else:
+    #             rebateList.append(lowVal_Pouche)
+    #     else:
+    #         if df['Material'][i] == MaterialList[2]:
+    #             rebateList.append(highVal_Pouche)
+    #         elif df['Material'][i] == MaterialList[3]:
+    #             rebateList.append(highVal_PET)
+    #         elif df['Material'][i] == MaterialList[4]:
+    #             rebateList.append(highVal_PET)
+    #         elif df['Material'][i] == MaterialList[5]:
+    #             rebateList.append(highVal_Plastic)
+    #         elif df['Material'][i] == MaterialList[6]:
+    #             rebateList.append(highVal_PET)
+    #         else :
+    #             rebateList.append(highVal_Pouche)
 
 
-    df['RebateValue'] = rebateList
-    df = df[df['UPC'].notna()]
-    UPC = barcode
+    # df['RebateValue'] = rebateList
+    # df = df[df['UPC'].notna()]
+    # UPC = barcode
 
-    UPC.strip()
-    UPC.replace(" ","")
+    # UPC.strip()
+    # UPC.replace(" ","")
 
-    if df['UPC'].str.contains(UPC).any():
-        idx = df.index.values[df['UPC'].str.contains(UPC)]
-        index = idx[0]
-        rebateVal = df['RebateValue'][index]
-        result = round(rebateVal, 3)
-        try:
-            all_users = [doc.to_dict() for doc in user_Ref.stream()]
-            for user in all_users:
-                if 'Name' in user and user['Name']=="Sarthak":
-                    user_Ref.document(str(8972342342)).update({"Rebate": firestore.Increment(float(result))})
-        except Exception as e:
-            return f"An Error Occured: {e}"
-        return True
+    # if df['UPC'].str.contains(UPC).any():
+    #     idx = df.index.values[df['UPC'].str.contains(UPC)]
+    #     index = idx[0]
+    #     rebateVal = df['RebateValue'][index]
+    #     result = round(rebateVal, 3)
+    #     try:
+    #         all_users = [doc.to_dict() for doc in user_Ref.stream()]
+    #         for user in all_users:
+    #             if 'Name' in user and user['Name']=="Sarthak":
+    #                 user_Ref.document(str(8972342342)).update({"Rebate": firestore.Increment(float(result))})
+    #     except Exception as e:
+    #         return f"An Error Occured: {e}"
+    #     return True
 
-    else:
-        return False
+    # else:
+    #     return False
 
 
 
